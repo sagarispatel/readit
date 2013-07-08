ba16d25d9b5a600b7174818b63b2a25e1a6d6cc0
MakerSquare Student <students@themakersquare.com>
2013-07-08 16:25:25 -0500

    Initial Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+
+
+  def index
+    @pages
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Readit::Application.config.secret_token = '0cd7338701ba99b473183c7058b1f1e53e19ac78134c73645b3f4a1b0ca3add7ba67e6e983d86cb6746c4445a02acbef02abe6336484e9f2d450157596809324'
