bce31d7128354b4ae5eefc852a20eb7715e6ab90
MakerSquare Student <students@themakersquare.com>
2013-07-08 16:39:55 -0500

    I forgot to remove index.html, so i did. and then i did some other shit

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.assets.initialize_on_precompile = false
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -35,7 +35,6 @@ module Readit
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
@@ -58,5 +57,9 @@ module Readit
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
+
   end
+
 end
