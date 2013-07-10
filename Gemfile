c81c491bffc7c11346e21a0e46ac694f03dd6f85
MakerSquare Student <students@themakersquare.com>
2013-07-10 14:51:32 -0500

    Add links to homepage

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'rails', '3.2.13'
 
 gem 'devise'
 gem 'haml'
+gem 'will_paginate'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
 
-
   def index
-    @pages
+    params[:per_page] ||= 10
+    params[:page]     ||= 1
+    #nil || 2 => 2
+    @links = Link.order('created_at DESC').page(params[:page]).per_page(params[:per_page])
   end
 
 end
