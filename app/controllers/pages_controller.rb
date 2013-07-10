f1d32bb41ebf8c543dd2fdf99c37cd5280fb93e4
MakerSquare Student <students@themakersquare.com>
2013-07-10 15:02:24 -0500

    forgot to add .; still pagiated hard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
 
   def index
-    params[:per_page] ||= 10
+    params[:per_page] ||= 3
     params[:page]     ||= 1
     #nil || 2 => 2
-    @links = Link.order('created_at DESC').page(params[:page]).per_page(params[:per_page])
+    @links = Link.order('created_at DESC').page(params[:page].to_i).per_page(params[:per_page].to_i)
   end
 
 end
